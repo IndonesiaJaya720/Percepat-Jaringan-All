c6a65ba3a03b655b3d5f4b75316aeec9d66b6361
IndonesiaJaya720 <72527640+IndonesiaJaya720@users.noreply.github.com>
2020-12-08 18:50:23 +0700

    Add files via upload

---
+++ WifiAll.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+#version 5.0
+
+
+#JANGAN RECONDING
+#RECONDING SCRIPT GUA
+#KONTOL
+#MAU CODING
+#BIKIN SENDIRI
+#KAWAN
+#JANGAN BETAK
+
+#AMIN
+
+
+blue='\33[0;36m'
+cyan='\e[0;36m'
+purple='\e[0;35m'
+brown='\e[0;33m'
+lightgray='\e[0;37m'
+darkgray='\e[1;30m'
+lightblue='\e[1;34m'
+lightgreen='\e[1;32m'
+lightcyan='\e[1;36m'
+lightred='\e[1;31m'
+lightpurple='\e[1;35m'
+yellow='\e[1;33m'
+white='\e[1;37m'
+red='\33[31;1m'
+green='\33[0;32m'
+black='\33[30;1m'
+sleep 1
+echo
+toilet -f big -F gay By [ BOS ]
+echo $black"Welcome"
+echo
+sleep 1
+echo $red"Siapa Install BOS"
+echo
+echo $red"Silakan Install BOS :"
+echo
+echo $red"Pilih Pakai Nomer Ya"
+echo
+echo $black"[~].Wifi IndiHome [~]"
+echo
+echo $black"[√].Wifi Modem Huawei All [√]"
+echo
+echo $black"[∆].Wifi Smartfren Andromax [∆]"
+echo
+echo $black"[=].Wifi ZTE MF920 All [=]"
+echo
+echo $black"[@].Wifi All [@]"
+echo
+echo $black"[0].Exit [0]"
+echo
+echo $blue"[7].Masuk Webset Gua [7]"
+echo
+echo $blue
+read -p "Pilih[BOS] : " Bos
+
+if [ $Bos = 1 ] || [ $Bos = 1 ]
+then
+clear
+toilet -f big -F [ Boss ]
+echo
+echo $black"Wifi IndiHome by Boss"
+echo
+pkg update && pkg upgrade
+ping -s 1000 205.116.250.180
+fi
+
+if [ $Bos = 2 ] || [ $Bos = 2 ]
+then
+clear
+toilet -f big -F [ Boss ]
+echo $green"Wifi Modem Huawei All"
+pkg update && pkg upgrade
+ping -s 1000 192.168.8.1
+fi
+
+if [ $Bos = 3 ] || [ $Bos = 3 ]
+then
+clear
+toilet -f big -F [ Boss ]
+echo $white"Wifi Smartfren Andromax"
+echo "[√]"
+pkg update && pkg upgrade
+echo "[√]"
+ping -s 1000 
+fi
+
+if [ $Bos = 4 ] || [ $Bos = 4 ]
+then
+toilet -f big -F [ Boss ]
+echo $lightred"Wifi ZTE MF920 All"
+echo $red
+echo [•]
+pkg update && pkg upgrade
+echo $white
+echo [•]
+ping -s 1000 
+fi
+
+if [ $Bos = 5 ] || [ $Bos = 5 ]
+then
+toilet -f big -F gay Boss
+echo $lightpurple"Wifi All"
+echo $red
+echo [√]
+pkg update && pkg upgrade
+echo $red
+echo [√]
+ping -s 1000 1.1.1.1
+fi
+
+if [ $Bos = 0 ] || [ $Bos = 0 ]
+then
+toilet -f big -F gay Boss
+echo $red"Exit"
+echo
+echo $red
+figlet [ BAY ]
+echo
+sleep 3
+clear
+echo $red
+figlet Terima Kasih
+echo
+sleep 3
+clear
+echo $red
+figlet MR_R5B
+echo
+sleep 3
+clear
+exit
+fi
+
+if [ $Bos = 7 ] || [ $Bos = 7 ]
+then
+toilet -f big -F [ Webset Gua ]
+echo $yellow"Webset Gua Coy"
+echo
+echo $yellow
+echo [•]
+echo
+echo $red
+echo [√]
+echo
+clear
+sleep 2
+pkg update
+pkg upgrade
+pkg install git -y
+pkg install bash && pkg install bash -y
+pkh install python && pkg install python2 -y
+git clone https://github.com/IndonesiaJaya720/WebsetGua
+ls
+cd WebsetGua
+ls
+python2 hasil.py
+fi
